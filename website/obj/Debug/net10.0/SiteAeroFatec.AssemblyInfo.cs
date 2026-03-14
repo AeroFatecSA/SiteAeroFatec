b835f78ea60e22c866ba9912144c652424022499
hugomontoro1 <hugomontoro1@gmail.com>
2026-03-14 05:19:05 -0300

    Finalizando integração C# e estrutura de membros.

--- website/obj/Debug/net10.0/SiteAeroFatec.AssemblyInfo.cs
+++ website/obj/Debug/net10.0/SiteAeroFatec.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SiteAeroFatec")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+166d5242782f909726e9b76af67cea0364aa886f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b1839c7e677094b956ac4e2ab913e3433b6cf0c")]
 [assembly: System.Reflection.AssemblyProductAttribute("SiteAeroFatec")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SiteAeroFatec")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
